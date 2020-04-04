55f3a491e3e0217c31c21b9a2735021bb5c2bc87
Francesco Puddu <10583315@polimi.it>
2020-04-04 11:20:35 +0200

    Implemented Observer and Observable with the legacy interfaces of java util. Also implemented a test class for Observable

---
+++ src/main/java/it/polimi/ingsw/Observer.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw;
+
+public interface Observer
+{
+    public void update(Object o);
+}
---
+++ src/test/java/it/polimi/ingsw/ObservableTest.java
@@ -0,0 +1,43 @@
+package it.polimi.ingsw;
+
+import org.junit.Test;
+
+public class ObservableTest
+{
+    public class ObserverUtil implements Observer
+    {
+        private boolean flag = false;
+
+        @Override
+        public void update(Object o)
+        {
+            this.flag = true;
+        }
+
+        public boolean getFlag()
+        {
+            return flag;
+        }
+    }
+
+    @Test
+    public void checkHasObservers()
+    {
+        Observable observable = new Observable();
+        ObserverUtil observer = new ObserverUtil();
+
+        observable.addObserver(observer);
+        assert(observable.hasObservers());
+    }
+
+    @Test
+    public void checkNotify()
+    {
+        Observable observable = new Observable();
+        ObserverUtil observer = new ObserverUtil();
+
+        observable.addObserver(observer);
+        observable.notify("test");
+        assert(observer.getFlag());
+    }
+}
