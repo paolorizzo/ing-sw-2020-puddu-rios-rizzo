22bfbe16a94819dacd635772ba23743779576750
Francesco Puddu <10583315@polimi.it>
2020-04-04 11:41:37 +0200

    Implemented more tests for Action class

--- src/test/java/it/polimi/ingsw/model/ActionTest.java
+++ src/test/java/it/polimi/ingsw/model/ActionTest.java
@@ -2,11 +2,47 @@ package it.polimi.ingsw.model;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ActionTest
 {
     @Test(expected = IllegalArgumentException.class)
-    public void validCoordinates()
+    public void checkValidCoordinates()
     {
         Action act = new Action("id", 3, 6);
+    }
+
+    @Test
+    public void checkWorker()
+    {
+        Action act = new Action("id", 1, 1);
+
+        assertEquals(act.getWorkerID(), "id");
+    }
+
+    @Test
+    public void checkGetCoordinates()
+    {
+        Action act = new Action("id", 1, 2);
+
+        assertEquals(act.getTargetX(), 1);
+        assertEquals(act.getTargetY(), 2);
+    }
+
+    @Test
+    public void checkToString()
+    {
+        Action act = new Action("id", 1, 2);
+
+        assertEquals(act.toString(), "Action{workerID='id', targetX=1, targetY=2}");
+    }
+
+    @Test
+    public void checkEquals()
+    {
+        Action act1 = new Action("id", 1, 2);
+        Action act2 = new Action("id", 1, 2);
+
+        assert(act1.equals(act2));
     }
 }
