db9530c4dbe637bb35b964e85fc1a4ea999bbe55
federico-rios <10567777@polimi.it>
2020-05-21 21:00:27 +0200

    Added a class and a folder that will be needed in the implementation of the persistence advanced functionality

---
+++ src/main/java/it/polimi/ingsw/model/PersistenceGame.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.model;
+
+import java.util.List;
+
+public class PersistenceGame {
+    int numPlayers;
+    int phase;
+    List<Card> cardsChosen;
+    int numCardsChosen;
+    int[] ids;
+    String[] names;
+    int[] numGods;
+}
+
+
