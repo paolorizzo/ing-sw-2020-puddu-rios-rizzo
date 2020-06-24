328fa84485404337ae639cb70b2f2a302e45e9b3
federico-rios <10567777@polimi.it>
2020-06-24 17:13:11 +0200

    Added documentation for the methods in UserInterfaceObserver

--- src/main/java/it/polimi/ingsw/observation/UserInterfaceObserver.java
+++ src/main/java/it/polimi/ingsw/observation/UserInterfaceObserver.java
@@ -7,13 +7,56 @@ import it.polimi.ingsw.model.Action;
  */
 public interface UserInterfaceObserver
 {
+    /**
+     * uses the ip the user chose
+     * @param ip the chosen ip address
+     */
     void updateReadIp(String ip);
+
+    /**
+     * uses the port that the user chose
+     * @param port the chosen port
+     */
     void updateReadPort(int port);
+
+    /**
+     * uses the number of players chosen by the user
+     * @param numPlayers the number of players
+     */
     void updateReadNumPlayers(int numPlayers);
+
+    /**
+     * uses the name the user chose
+     * @param name the chosen name
+     */
     void updateReadName(String name);
+
+    /**
+     * uses the intent of the player to restore a saved game or not
+     * @param restore
+     */
     void updateReadRestore(boolean restore);
+
+    /**
+     * uses the choice of card from the deck that the user made
+     * @param numCard the chosen card
+     */
     void updateReadNumCard(int numCard);
+
+    /**
+     * uses the choice of card from the subset of possible cards
+     * @param numCard the chosen card
+     */
     void updateReadGod(int numCard);
+
+    /**
+     * uses the action that the player chose
+     * @param action the chosen action
+     */
     void updateReadAction(Action action);
+
+    /**
+     * uses the choice to end the turn prematurely made by the user
+     */
     void updateReadVoluntaryEndOfTurn();
 }
