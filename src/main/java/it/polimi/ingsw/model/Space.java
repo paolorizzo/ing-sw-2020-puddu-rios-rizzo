4a1bfe3425f5fbc10afb7c7723833ada8b36a0f0
federico-rios <10567777@polimi.it>
2020-06-28 16:11:47 +0200

    Added full documentation for: Space

--- src/main/java/it/polimi/ingsw/model/Space.java
+++ src/main/java/it/polimi/ingsw/model/Space.java
@@ -11,6 +11,11 @@ public class Space {
     private List<Piece> pieces;
     private List<Space> adjacentSpaces;
 
+    /**
+     * constructs a Space object given its position on the board
+     * @param posX the x coordinate of the space
+     * @param posY the y coordinate of the space
+     */
     public Space(int posX, int posY) {
         this.posX = posX;
         this.posY = posY;
@@ -20,30 +25,59 @@ public class Space {
         worker = null;
     }
 
+    /**
+     * adds another space as adjacent to this one
+     * @param s the other space
+     */
     void addAdjacentSpace(Space s){
         adjacentSpaces.add(s);
     }
 
+    /**
+     * returns the list of spaces that are adjacent to this one
+     * @return the list of spaces that are adjacent to this one
+     */
     public List<Space> getAdjacentSpaces(){
         return adjacentSpaces;
     }
 
+    /**
+     * checks whether there is a worker on this space
+     * @return true if there is a worker on this space
+     */
     public boolean hasWorkerOnIt(){
         return worker != null;
     }
 
+    /**
+     * returns the worker on this space
+     * returns null if there are no workers
+     * @return the worker on this space. Can return null
+     */
     public Worker getWorkerOnIt(){
         return worker;
     }
 
+    /**
+     * sets a worker on this space
+     * @param w the worker to set
+     */
     void setWorkerOnIt(Worker w){
         this.worker = w;
     }
 
+    /**
+     * removes the worker from the space, if it is there
+     */
     void removeWorkerOnIt(){
         this.worker = null;
     }
 
+    /**
+     * adds a building level on top of this space, if
+     * it is logically coherent to do so
+     * @param p the piece to add
+     */
     void addPiece(Piece p){
         if(pieces.contains(p))
             throw new IllegalArgumentException("This space already contains" + p.toString());
@@ -53,6 +87,13 @@ public class Space {
             pieces.add(p);
     }
 
+    /**
+     * checks whether this space already contains a building
+     * of higher level than the given one
+     * @param p the given building level
+     * @return true if this space contains a building of higher
+     * level than the given one p
+     */
     private boolean UpperLevelInSpace(Piece p) {
         if (p != Piece.DOME && pieces.size() > 0)
         {
@@ -64,37 +105,70 @@ public class Space {
         return false;
     }
 
-    //to undo action
+    /**
+     * removes the highest piece from the space
+     */
     void removeLastPiece(){
         if(pieces.size() == 1)
             throw new IllegalArgumentException("Can't undo removing piece level 0");
         pieces.remove(pieces.size()-1);
     }
+
+    /**
+     * returns the highest piece on the space
+     * @return the highest piece on the space
+     */
     public Piece getLastPiece(){
         return pieces.get(pieces.size()-1);
     }
 
+    /**
+     * returns the level of the highest piece on the space
+     * @return the level of the highest piece on the space
+     */
     public int getLevel(){
         Piece last = pieces.get(pieces.size()-1);
         return last.getLevel();
     }
 
+    /**
+     * returns the x coordinate of the space
+     * @return the x coordinate of the space
+     */
     public int getPosX() {
         return posX;
     }
 
+    /**
+     * returns the y coordinate of the space
+     * @return the y coordinate of the space
+     */
     public int getPosY() {
         return posY;
     }
 
+    /**
+     * checks whether a worker can move on this space
+     * @return true if a worker can move on this space
+     */
     public boolean isFreeSpace(){
         return (!this.hasWorkerOnIt()) && (this.getLevel() != 4);
     }
 
+    /**
+     * checks whether this is a peripheral space
+     * @return true if this is a peripheral space
+     */
     public boolean isPeripheralSpace(){
         return posX == 0 || posX == 4 || posY == 0 || posY == 4;
     }
 
+    /**
+     * clones and returns another space, identical
+     * except with regards to the adjacent space
+     * @return a copy of this space, identical except
+     * without its adjacent spaces
+     */
     protected Space lightClone(){
         Space space = new Space(posX, posY);
         for(Piece piece: pieces)
@@ -102,6 +176,12 @@ public class Space {
         return space;
     }
 
+    /**
+     * compares another object to this space
+     * @param o the other object
+     * @return true if the other object is a Space
+     * and its position matches that of this space
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
